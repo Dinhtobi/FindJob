57a7c79cf3dcd3c1a0bbecbfefb4e8758477d144
Tobi <dinhnguyen2002asd@gmail.com>
2024-03-10 00:08:30 +0700

    Chat bot deploy

--- server.sh
+++ server.sh
@@ -2,4 +2,4 @@ cd app/ && rasa run --model /app/models --enable-api --cors "*" --debug -p $PORT
 
 cd /app && rasa run actions -p $PORT
 
-cd /app && python app -p $PORT
+# cd /app && python app -p $PORT
